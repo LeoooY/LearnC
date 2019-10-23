dc0ab23e5b618cc37dfc8446b38f334698b5fd2a
LeoooY <Leoyy3@163.com>
2019-10-23 18:40:08 +0800

    C实现一个简单的计算

--- practice/CaiNiaoExample/2.printInput/index.c
+++ practice/CaiNiaoExample/2.printInput/index.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     int number;
-    printf("请输入一个整数：");
-    
+    printf("please int a Int:");
+
     // 获取输入的函数，和printf同样由stdio.h引入
     // %d 用于将输入格式化为整数
     // &number 用于将输入赋值给number变量
-    scanf("%d",&number);
+    scanf("%d", &number);
 
     // %d 在这里用于格式化输出
-    printf("你输入的整数是： %d",number);
+    printf("你输入的整数是： %d", number);
 
     return 0;
 }

40d9ed6632e2be184cb8188a12353fac6c18828e
LeoooY <Leoyy3@163.com>
2019-10-24 09:50:10 +0800

    easySort optimise

--- practice/CaiNiaoExample/5.easySort/index.c
+++ practice/CaiNiaoExample/5.easySort/index.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 // 题目：输入三个整数x,y,z，请把这三个数由小到大输出。
 
-
 /* 智障版本 */
 /* 用一个中间数tmp,将变量进行交换 */
 // int main(){
@@ -20,31 +19,40 @@
 //     printf("\nmin to max: %d, %d, %d",x,y,z);
 // }
 
+/*  稍微高级点的版本，用数组来存，用数组排序 */
 
-/* 稍微高级点的版本，用数组来存，用数组排序 */
-int main(){
+/*  插入时候使用冒泡排序 */
 
+/*
+    每个数字的输入是O(n)的时间复杂度，在每次输入时候进行冒泡排序也是o(n)的时间复杂度
+    总体复杂度O(n^2)
+*/
+
+/*
+    优化：
+    先输入再使用快排的话，复杂度为n+nlog(n)
+*/
+int main() {
     int arr[5];
     int input;
     int swap;
     printf("Input three numbers, like: 1,2,3\n");
 
-    for(int i=0; i<5; i++){
-        printf("Input %dth number:\n ",i+1);
-        scanf("%d",&input);        
-        arr[i]=input;
+    for (int i = 0; i < 5; i++) {
+        printf("Input %dth number:\n ", i + 1);
+        scanf("%d", &input);
+        arr[i] = input;
 
-        swap=i;
-        while(swap>0 && arr[swap]<arr[swap-1]){
-            int tmp=arr[swap];
-            arr[swap]=arr[swap-1];
-            arr[swap-1]=tmp;            
+        swap = i;
+        while (swap > 0 && arr[swap] < arr[swap - 1]) {
+            int tmp = arr[swap];
+            arr[swap] = arr[swap - 1];
+            arr[swap - 1] = tmp;
             swap--;
-        }        
+        }
     }
 
-    for(int i; i<5;i++){
-        printf("%d,",arr[i]);
+    for (int i; i < 5; i++) {
+        printf("%d,", arr[i]);
     }
-    
 }

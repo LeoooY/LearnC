1513a54783b739fd8e1a2854c6478cbdc9d6a866
LeoooY <leoY.dev@gmail.com>
2020-01-09 10:45:23 +0800

    understand c pointer with array and &

--- practice/test.c
+++ practice/test.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 
-int main(){
-    char a[]="hello";
-    char *pa=&a[0];
-    char *p="hello";
-    char *pp=p;
-    if(pp==p){
-        printf("Hello\n");
-    }    
- }
+int main() {
+    char a[] = "hello";
+    char *pa = &a[0];
+    char *p = "hello";
+    char *pp = p;
+    
+    if (&a[0] == a && a==&a ) {
+        printf("&a[0] == a == &a\n");
+    }
+
+    if ((&a + 1) == (a + 1)) {
+        printf("&a+1 == a+1 \n");
+    }else{
+        printf("&a+1 != a+1 \n");
+    }
+}

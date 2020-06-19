c0c405235dabf09f410d6d86a087ebebb134faff
LeoooY <leoy.dev@gmail.com>
2020-06-19 18:54:29 +0800

    cpp refer point

--- cpp/demos/testP.cpp
+++ cpp/demos/testP.cpp
@@ -8,5 +8,15 @@ int main() {
     int *pVal = &val;  // 声明并初始化指针
     int *pVal2 = &refVal;   // 当指针取引用的地址时候，实际上取到的refVal引用的变量，即&refVal 等价于 &val
     cout << *pVal << *pVal2 << refVal << endl;
+
+
+    int i = 42;
+    // int &r = i;     // &紧随类型名出现，因此是声明的一部分,r是一个引用
+    int *p;         // *紧随类型名出现，因此是声明的一部分,p是一个指针
+    p = &i;         // &在表达式中出现，是一个取地址符，指针p指向变量i的地址
+    *p = i;         // *出现在表达式中，是一个解引用符，指针p的地址赋值为i的值
+    int &r2 = *p;   // &是声明的一部分，*是一个解引用符，引用rs绑定到*p解引用的结果i上，等价于 int &r2=i;
+    bool rs= bool(*p == i); // true    
+    cout << r2 << " " << rs;
     return 0;
 }
--- cpp/demos/waiting.cpp
+++ cpp/demos/waiting.cpp
@@ -1,17 +1,27 @@
-#include <iostream>
 #include <ctime>
+#include <iostream>
 
-int main(){
-    http://www.google.com/
+int main() {
+http: // www.google.com/
     using namespace std;
     float secs;
-    cout << "请输入等待时间（单位秒s）"<< endl;
+    cout << "请输入等待时间（单位秒s）" << endl;
     cin >> secs;
-    clock_t delay=secs* CLOCKS_PER_SEC;
+    clock_t delay = secs * CLOCKS_PER_SEC;
     cout << "starting " << delay << "," << secs << endl;
-    clock_t start =clock();
-    while(clock()-start<delay)
+    clock_t start = clock();
+    while (clock() - start < delay)
         ;
     cout << "done \a\n";
     return 0;
+}
+
+void test() {
+
+    int i = 42;
+    int &r = i;     // &紧随类型名出现，因此是声明的一部分,r是一个引用
+    int *p;         // *紧随类型名出现，因此是声明的一部分,p是一个指针
+    p = &i;         // &在表达式中出现，是一个取地址符，指针p指向变量i的地址
+    *p = i;         // *出现在表达式中，是一个解引用符，指针p的地址赋值为i的值
+    int &r2 = *p;   // 
 }
